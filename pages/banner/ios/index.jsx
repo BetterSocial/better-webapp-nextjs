cf548d806b0798fd62bf63e1afaf0d2f431b9e0f
Muhammad Fajariansyah Ismail <fajarism@RokaM1.local>
2023-01-28 14:38:44 +0700

    fix: next lint error

--- pages/banner/ios/index.jsx
+++ pages/banner/ios/index.jsx
@@ -4,7 +4,7 @@ import { BannerPage } from "component/Page/BannerPage"
 import { Helmet } from "react-helmet"
 
 export default function IOSBanner(props) {
-    const bannerImage = <a href='https://apps.apple.com/us/app/better-social/id1615684520' target="_blank">
+    const bannerImage = <a href='https://apps.apple.com/us/app/better-social/id1615684520' target="_blank" rel="noreferrer">
         <Image className="w-48 mt-12" src="/image/app_store_badge.svg" alt="Download on the App Store" width="480" height="200" />
     </a>
 
