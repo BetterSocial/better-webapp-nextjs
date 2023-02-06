0d723e921c0987b65675eff0b61db45a1d23d293
Muhammad Fajariansyah Ismail <fajarism@RokaM1.local>
2023-02-06 10:12:52 +0700

    fix: android link and ios banner height

--- component/Brand/IOSBannerImage.js
+++ component/Brand/IOSBannerImage.js
@@ -3,6 +3,6 @@ import Image from "next/image";
 
 export default function IOSBannerImage() {
     return <a href={Constant.Link.appstore} target="_blank" rel="noreferrer">
-        <Image className="w-40" src="/image/app_store_badge.svg" alt="Download on the App Store" width="480" height="200" />
+        <Image className="w-[149px]" src="/image/app_store_badge.svg" alt="Download on the App Store" width="480" height="200" />
     </a>
 }
