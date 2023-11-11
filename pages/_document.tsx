da797fe5c7fb7a1d158107bd0f2c16bf1369bc9c
Yusuf Putra <yusufputra99@gmail.com>
2023-11-11 17:29:14 +0700

    fix: layout and viewport mobile first

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,8 @@ export default function Document() {
     return (
         <Html>
             <Head>
+                {/* Modified meta[name="viewport"] tag with viewport-fit=cover */}
+                <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
                 <link href='https://fonts.googleapis.com/css?family=Lato:400,700' rel='stylesheet' type='text/css' />
             </Head>
             <body>
